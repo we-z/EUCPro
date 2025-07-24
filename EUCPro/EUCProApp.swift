934aa15ad2f3316d36017e69dca77cf134903e90
we-z <yousefsalem00@gmail.com>
2025-07-24 14:35:46 -0700

    Initial Commit

---
+++ EUCPro/EUCProApp.swift
@@ -0,0 +1,17 @@
+//
+//  EUCProApp.swift
+//  EUCPro
+//
+//  Created by Wheezy Capowdis on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct EUCProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EUCProTests/EUCProTests.swift
@@ -0,0 +1,17 @@
+//
+//  EUCProTests.swift
+//  EUCProTests
+//
+//  Created by Wheezy Capowdis on 7/24/25.
+//
+
+import Testing
+@testable import EUCPro
+
+struct EUCProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

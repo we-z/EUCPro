df2b364d54133c1e147a95d06639165fd80987bf
we-z <yousefsalem00@gmail.com>
2025-08-01 01:16:21 -0700

    new icon, new alpha smoothing

--- EUCPro/Views/TempView.swift
+++ EUCPro/Views/TempView.swift
@@ -1,46 +1,113 @@
-//
-//  TempView.swift
-//  EUCPro
-//
-//  Created by Wheezy Capowdis on 7/27/25.
-//
-
 import SwiftUI
+import CoreLocation
 import Charts
 
-struct DataPoint: Identifiable {
-    var id = UUID()
-    let x: Double
-    let y: Double
+class MotionLocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
+    private var displayLink: CADisplayLink?
+
+    @Published var smoothedSpeed: Double = 0.0
+    @Published var gpsSpeed: Double = 0.0
+
+    @Published var smoothedSpeedHistory: [(time: TimeInterval, value: Double)] = []
+    @Published var gpsSpeedHistory: [(time: TimeInterval, value: Double)] = []
+
+    private var startTime: TimeInterval = Date().timeIntervalSince1970
+    private var lastGPSSpeedMps: Double = 0.0
+    private var filteredSpeedMps: Double = 0.0
+
+    override init() {
+        super.init()
+        setupLocation()
+        setupUpdateLoop()
+    }
+
+    private func relativeTime() -> TimeInterval {
+        return Date().timeIntervalSince1970 - startTime
+    }
+
+    private func setupLocation() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let gpsSpeedMps = max(location.speed, 0)
+        lastGPSSpeedMps = gpsSpeedMps
+
+        let mph = gpsSpeedMps * 2.23694
+        let t = relativeTime()
+
+        DispatchQueue.main.async {
+            self.gpsSpeed = mph
+            self.gpsSpeedHistory.append((t, mph))
+        }
+    }
+
+    private func setupUpdateLoop() {
+        displayLink = CADisplayLink(target: self, selector: #selector(updateSmoothedSpeed))
+        displayLink?.preferredFramesPerSecond = 100
+        displayLink?.add(to: .main, forMode: .default)
+    }
+
+    @objc private func updateSmoothedSpeed() {
+        // Low-pass filter
+        let alpha = 0.1
+        filteredSpeedMps = alpha * lastGPSSpeedMps + (1 - alpha) * filteredSpeedMps
+
+        let mph = max(0, filteredSpeedMps * 2.23694)
+        let t = relativeTime()
+
+        DispatchQueue.main.async {
+            self.smoothedSpeed = mph
+            self.smoothedSpeedHistory.append((t, mph))
+        }
+    }
 }
 
-let minGlobalX: Double = 0
-let maxGlobalX: Double = 100
-
-let data: [DataPoint] = (Int(minGlobalX) ..< Int(maxGlobalX)).map { DataPoint(x: Double($0), y: Double(arc4random()) / Double(UInt32.max)) }
-
 struct TempView: View {
-    @State private var isDragging = false
-        @State private var numbers = (0...10).map { _ in
-            Int.random(in: 0...10)
-        }
-        
-        var body: some View {
-            Chart {
-                ForEach(Array(zip(numbers, numbers.indices)), id: \.0) { number, index in
-                    LineMark(
-                        x: .value("Index", index),
-                        y: .value("Value", number)
-                    )
-                    .foregroundStyle(isDragging ? .red : .blue)
-                }
+    @StateObject private var manager = MotionLocationManager()
+
+    var body: some View {
+        VStack(spacing: 24) {
+            VStack {
+                Text("Live Speed")
+                    .font(.headline)
+                Text(String(format: "%.2f MPH", manager.smoothedSpeed))
+                    .font(.system(size: 48, weight: .bold, design: .rounded))
+                    .foregroundColor(.blue)
             }
-            .gesture(
-                DragGesture()
-                    .onChanged { _ in isDragging = true }
-                    .onEnded { _ in isDragging = false }
-            )
+
+            VStack(alignment: .leading) {
+                Text("Smoothed Speed (MPH)")
+                    .font(.headline)
+                Chart {
+                    ForEach(manager.smoothedSpeedHistory, id: \.time) { point in
+                        LineMark(x: .value("Time", point.time),
+                                 y: .value("Speed", point.value))
+                    }
+                }
+                .frame(height: 150)
+            }
+
+            VStack(alignment: .leading) {
+                Text("GPS Speed (MPH)")
+                    .font(.headline)
+                Chart {
+                    ForEach(manager.gpsSpeedHistory, id: \.time) { point in
+                        LineMark(x: .value("Time", point.time),
+                                 y: .value("Speed", point.value))
+                    }
+                }
+                .frame(height: 150)
+            }
         }
+        .padding()
+    }
 }
 
 #Preview {
